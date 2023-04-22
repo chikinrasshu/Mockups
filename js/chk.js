975cc3d4ddd6ec72910dc196199e95b84cc29f34
Jorge Botarro <chikinrasshu@icloud.com>
2023-04-22 19:29:06 -0400

    Validaciones en Login y Register completas.

--- js/chk.js
+++ js/chk.js
@@ -1,33 +1,21 @@
-function toggleUserMenu(parent, menu) {
-    console.log(parent);
-    parent.classList.toggle("shown");
-    menu.classList.toggle("shown");
-}
+(() => {
+    'use strict'
 
-document.addEventListener("DOMContentLoaded", function () {
-    let userMenu = document.querySelectorAll(".user-menu");
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+    console.log(forms);
 
-    console.log("Buttons: ");
-    userMenu.forEach(menu => {
-        let btn = menu.querySelector("button");
-        let submenu = menu.querySelector("ul");
-        if (submenu.classList.contains("shown")) {
-            menu.classList.add("shown");
-        }
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            console.log("Checking validity");
+            if (!form.checkValidity()) {
+                console.log("Is valid?");
+                event.preventDefault();
+                event.stopPropagation();
+            }
 
-        btn.addEventListener("click", function (e) {
-            toggleUserMenu(menu, submenu);
-        });
-    });
-
-    // Go back links
-    let backs = document.querySelectorAll('.go-back');
-    backs.forEach(e => {
-        e.setAttribute("href", document.referrer);
-        e.onclick = function () {
-            history.back();
-            return false;
-        };
-    });
-
-});
+            form.classList.add('was-validated');
+        }, false)
+    })
+})()
